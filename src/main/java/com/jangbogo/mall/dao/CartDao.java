90a4124aaafc7056482e8a311951c1aaa70e7883
djdu4496 <djdu4496@gmail.com>
2023-04-10 08:52:45 +0900

    refactor : 장바구니 컨트롤러, 서비스, 다오 인터페이스 추출

--- src/main/java/com/jangbogo/mall/dao/CartDao.java
+++ src/main/java/com/jangbogo/mall/dao/CartDao.java
@@ -1,64 +1,17 @@
 package com.jangbogo.mall.dao;
 
 import com.jangbogo.mall.domain.CartDto;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Repository
-public class CartDao {
-    // SqlSession 자동 주입
-    @Autowired
-    SqlSession session;
+public interface CartDao {
+    CartDto getUserIdx(String email) throws Exception;
 
-    private static String namespace="com.jangbogo.mall.dao.CartMapper."; // 대소문자 구분X
+    List<CartDto> selectAll(Integer user_idx) throws Exception;
 
-    public CartDto getUserIdx(String email) throws Exception {
-        return session.selectOne(namespace + "selectUserIndex", email);
-    }
+    int delete(Integer prod_idx, Integer user_idx) throws Exception;
 
-    public List<CartDto> selectAll(Integer user_idx) throws Exception {
-        // SqlSession의 selectList메서드 호출(namespace, 아이디명 인자로 지정)
-        return session.selectList(namespace + "selectAll", user_idx);
-    }
+    int addCount(Integer prod_idx, Integer user_idx) throws Exception;
 
-    public int delete(Integer prod_idx, Integer user_idx) throws Exception {
-        // 1. 매개변수로 받은 두 개의 값을 저장할 객체 저장소 map 선언 및 HashMap 할당
-        Map map = new HashMap();
-        // 2. HashMap에 prod_idx를 k/v로 저장
-        map.put("prod_idx", prod_idx);
-        // 3. HashMap에 user_idx를 k/v로 저장
-        map.put("user_idx", user_idx);
-        // 4. SqlSession의 delete메서드 호출(namespace, 아이디명, map 인자로 지정)
-        return session.delete(namespace + "delete", map);
-    }
-
-    public int addCount(Integer prod_idx, Integer user_idx) throws Exception {
-        // 1. 매개변수로 받은 두 개의 값을 저장할 객체 저장소 map 선언 및 HashMap 할당
-        Map map = new HashMap();
-        // 2. HashMap에 prod_idx를 k/v로 저장
-        map.put("prod_idx", prod_idx);
-        // 3. HashMap에 user_idx를 k/v로 저장
-        map.put("user_idx", user_idx);
-        // 4. SqlSession의 update메서드 호출(namespace, 아이디명, map 인자로 지정)
-        return session.update(namespace + "addCount", map);
-    }
-
-    public int subtractCount(Integer prod_idx, Integer user_idx, Integer prod_cnt) throws Exception {
-        // 1. 매개변수로 받은 두 개의 값을 저장할 객체 저장소 map 선언 및 HashMap 할당
-        Map map = new HashMap();
-        // 2. HashMap에 prod_idx를 k/v로 저장
-        map.put("prod_idx", prod_idx);
-        // 3. HashMap에 user_idx를 k/v로 저장
-        map.put("user_idx", user_idx);
-        // 4. HashMap에 prod_idx를 k/v로 저장
-        map.put("prod_cnt", prod_cnt);
-
-        // 5. SqlSession의 update메서드 호출(namespace, 아이디명, map 인자로 지정)
-        return session.update(namespace + "subtractCount", map);
-    }
+    int subtractCount(Integer prod_idx, Integer user_idx, Integer prod_cnt) throws Exception;
 }
