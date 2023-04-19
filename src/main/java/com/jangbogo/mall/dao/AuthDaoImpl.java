96dbc95a37a144e1287c54bfb583a7f927bff01c
jinvicky <jinvicky17@gmail.com>
2023-04-19 21:01:38 +0900

    Feat: 권한 조회 기능 추가

--- src/main/java/com/jangbogo/mall/dao/AuthDaoImpl.java
+++ src/main/java/com/jangbogo/mall/dao/AuthDaoImpl.java
@@ -1,4 +1,22 @@
 package com.jangbogo.mall.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class AuthDaoImpl implements AuthDao{
+
+    @Autowired
+    SqlSession session;
+
+    private static String nameSpace = "com.fastcampus.ch4.dao.AuthMapper.";
+
+    @Override
+    public List<String> getAuthList(int auth_idx) throws Exception{
+        return session.selectList(nameSpace + "getAuthList", auth_idx);
+    }
+
 }
