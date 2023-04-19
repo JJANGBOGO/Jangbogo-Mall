fd35f4bee44fa8f11e2ffcec22c5e8f6402c3344
djdu4496 <djdu4496@gmail.com>
2023-04-19 23:58:13 +0900

    feat : KakaoReadyRequestDto 생성

---
+++ src/main/java/com/jangbogo/mall/domain/KakaoReadyRequestDto.java
@@ -0,0 +1,50 @@
+package com.jangbogo.mall.domain;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class KakaoReadyRequestDto {
+    // iv
+    private String item_name;               // 주문상품명 "그레놀라 외 1개
+    private Integer quantity;               // 주문상품수량
+    private Integer total_amount;           // 주문총금액
+
+    // constructor
+    public KakaoReadyRequestDto() {};       // 기본 생성자
+
+    // getter & setter
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(Integer total_amount) {
+        this.total_amount = total_amount;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "{" +
+                "item_name='" + item_name + '\'' +
+                ", quantity=" + quantity +
+                ", total_amount=" + total_amount +
+                '}';
+    }
+}
