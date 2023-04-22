31bada2d3216480cc7cb1376506f85d00126e501
djdu4496 <djdu4496@gmail.com>
2023-04-23 03:06:00 +0900

    edit : Model에 담아 View(orderSuccess)에 Dto를 전달하는 방식을 RedirectAttributes에 담아 전달하는 방식으로 변경

--- src/main/java/com/jangbogo/mall/controller/KakaoPayController.java
+++ src/main/java/com/jangbogo/mall/controller/KakaoPayController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
@@ -39,9 +40,9 @@ public class KakaoPayController {
     // 메서드명 : approveKakaoPayRequest
     // 기   능 : 카카오페이 결제 승인 요청 처리
     // 반환타입 : String
-    // 매개변수 : String pg_token, Model model
+    // 매개변수 : @RequestParam("pg_token") String pg_token, RedirectAttributes rattr, HttpSession session
     @GetMapping("/payment/kakao/approve")
-    public String approveKakaoPayRequest(@RequestParam("pg_token") String pg_token, Model model, HttpSession session) throws Exception {
+    public String approveKakaoPayRequest(@RequestParam("pg_token") String pg_token, RedirectAttributes rattr, HttpSession session) throws Exception {
         OrderDto orderDto = (OrderDto)session.getAttribute("orderDto");                                           // 변수명 : orderDto - 저장값 : 세션에 저장되어 있는 주문 데이터 객체(orderDto)
         int insertOrderDtoRowCnt = 0;                                                                                   // 변수명 : insertOrderDtoRowCnt - 저장값 : '주문' 테이블에 데이터 저장
         int insertPaymentDtoRowCnt = 0;                                                                                 // 변수명 : insertPaymentDtoRowCnt - 저장값 : '결제' 테이블에 데이터 저장
@@ -63,14 +64,17 @@ public class KakaoPayController {
             if(insertPaymentDtoRowCnt == 0) throw new Exception("insert PaymentDto to 'SETL' Table failed!");           // 데이터 저장 실패 시, 예외 발생
             cartService.removeAll(orderDto.getUser_idx());                                                              // '주문완료' 처리 시, 장바구니 목록 초기화
 
-            model.addAttribute("model", kakaoApproveResponseDto);                                          // 뷰에 전달할 데이터를 모델(model)에 저장
+//          model.addAttribute("model", kakaoApproveResponseDto);                                                       // 뷰에 전달할 데이터를 모델(model)에 저장
+            rattr.addFlashAttribute("model", kakaoApproveResponseDto);
             session.removeAttribute("orderDto");                                                                  // 세션에 저장되어 있는 주문 데이터 객체(orderDto) 삭제
             session.removeAttribute("paymentDto");                                                                // 세션에 저장되어 있는 결제 데이터 객체(paymentDto) 삭제
+            return "redirect:/order/checkout/success";
         } catch (Exception e) {                                                                                         // 예외 처리
             e.printStackTrace();
-            throw new Exception("approve KakaoPay request failed!");                                                    // 결제 승인 처리 실패 시, 예외 발생
+//          throw new Exception("approve KakaoPay request failed!");                                                    // 결제 승인 처리 실패 시, 예외 발생
+            return "redirect:/order/checkout";
         }
-        return "order/orderSuccess";                                                                                    // 뷰 이동
+//      return "order/orderSuccess";                                                                                    // 뷰 이동
 
     }
 
