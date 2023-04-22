611eecfca5bd9a92a31d1c035595a3c42af63d53
jinvicky <jinvicky17@gmail.com>
2023-04-22 16:34:12 +0900

    Feat: 유효성 검사 1차 개발

--- src/main/webapp/resources/js/member/regProduct.js
+++ src/main/webapp/resources/js/member/regProduct.js
@@ -1,4 +1,4 @@
-function validateProduct () {
+function validateProduct() {
     let prod_cd = $("#seler_prod_cd"); //상품 코드
     if (prod_cd.val() === "") {
         alert("상품 코드를 입력해 주세요");
@@ -9,7 +9,7 @@ function validateProduct () {
     //카테고리 잘 모르겠슈...
 
     let name = $("#name"); //상품 이름
-    if ($.trim(name.val()) === "") {
+    if ($.trim(name.val()) === "") { //text
         alert("상품 이름을 입력해 주세요");
         name.focus();
         return false;
@@ -22,7 +22,7 @@ function validateProduct () {
     }
 
     let ctent = $("#ctent"); //상품 설명
-    if ($.trim(ctent.val()) === "") {
+    if ($.trim(ctent.val()) === "") { //text
         alert("상품 설명을 입력해 주세요");
         ctent.focus();
         return false;
@@ -37,6 +37,31 @@ function validateProduct () {
 
     //대표 이미지 업로드...
 
+    let dc_state_cd = $("input[name=dc_state_cd]:checked").val();
+    //할인율 radio
+    let dc_rate = $("#dc_rate");
+    const dc_applied = "1"; //할인 적용 여부
+
+
+    if (dc_state_cd === "") { //검사기에서 악성으로 제거한 경우
+        alert("할인 여부를 선택해 주세요");
+        return false;
+    }
+
+    if (dc_state_cd === dc_applied && dc_rate.val() === "") {
+        alert("할인율을 입력해 주세요");
+        dc_rate.focus();
+        return false;
+    }
+
+    let dsply_state_cd = $("input[name=dsply_state_cd]:checked").val();
+
+    if (dsply_state_cd === "") {
+        alert("전시 상태를 선택해 주세요");
+        return false;
+    }
+
+    let origin = $(); //원산지
 
     return true;
 }
